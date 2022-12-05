f2086738ab29ad99c2e257fc057906520be7d327
SikuDev <68606343+SikuDev@users.noreply.github.com>
2022-12-05 13:15:42 -0500

    All done except timers - super janky atm

--- scoreboard/_index.js
+++ scoreboard/_index.js
@@ -4,32 +4,14 @@ const guestScoreEl = document.getElementById("guest-score");
 const homeScoreStyle = document.querySelector(".side-column.home > .score");
 const guestScoreStyle = document.querySelector(".side-column.guest > .score");
 
-const homeFoulsEl = document.querySelector("#home-fouls");
-const guestFoulsEl = document.querySelector("#guest-fouls");
-
-const timerEl = document.querySelector("#timer");
-const periodEl = document.querySelector("#period");
-
-let playing = false;
-let paused = false;
+const homeFoulsEl = document.querySelector("#home-fouls")
+const guestFoulsEl = document.querySelector("#guest-fouls")
 
 function reset() {
 	homeScoreEl.textContent = 0;
 	guestScoreEl.textContent = 0;
-	homeFoulsEl.textContent = 0;
-	guestFoulsEl.textContent = 0;
-  playing = false;
-  paused = true;
-  timerEl.textContent = "00:00";
-  periodEl.textContent = 0;
-
-  const interval_id = window.setInterval(function () {},
-  Number.MAX_SAFE_INTEGER);
-
-  for (let i = 0; i < interval_id; i++) {
-    window.clearInterval(i);
-  }
-
+  homeFoulsEl.textContent = 0;
+  guestFoulsEl.textContent = 0;
 	highlightLeader();
 }
 
@@ -76,68 +58,6 @@ function highlightLeader() {
 			guestScoreStyle.style.borderColor = "#F94F6D";
 			guestScoreStyle.style.boxShadow = "0 0 10px 0 red";
 	}
-}
-
-function startTimer(duration) {
-	let time = duration * 60000;
-
-	if (!playing) {
-		playing = true;
-		paused = false;
-		displayTime(time);
-		if (time == duration * 60000 && parseInt(periodEl.textContent) < 4) {
-			periodEl.textContent = parseInt(periodEl.textContent) + 1;
-		} else {
-			periodEl.textContent = 0;
-		}
-		setTimeout(() => {
-			playing = false;
-			paused = true;
-			timerEl.textContent = "00:00";
-			// Get a reference to the last interval + 1
-			const interval_id = window.setInterval(function () {},
-			Number.MAX_SAFE_INTEGER);
-
-			// Clear any timeout/interval up to that id
-			for (let i = 0; i < interval_id; i++) {
-				window.clearInterval(i);
-			}
-		}, duration * 60000);
-	}
-
-	if (playing) {
-		setInterval(() => {
-			if (time <= 0) {
-				paused = true;
-				playing = false;
-			} else {
-				time -= 1000;
-			}
-
-			displayTime(time);
-		}, 1000);
-	}
-}
-
-function pause() {
-	if (playing) {
-		paused = true;
-		playing = false;
-	}
-}
-
-function displayTime(time) {
-	let minutes = Math.floor(time / 1000 / 60);
-	let seconds = (time / 1000) % 60;
-
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-
-	timerEl.textContent = minutes + ":" + seconds;
 }
 
 reset();
