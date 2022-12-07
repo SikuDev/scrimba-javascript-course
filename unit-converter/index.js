a780f66bee74fdec3fa38166910d370468977332
SikuDev <68606343+SikuDev@users.noreply.github.com>
2022-12-07 10:57:02 -0500

    Uploaded Unit Converter

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,8 +1,25 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
-        <script src="index.js"></script>
-    </body>
-</html>
+const convertBtn = document.getElementById("convert-btn")
+
+const inputEl = document.getElementById("input-el")
+
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+convertBtn.addEventListener("click", function(){
+  let inputNum = inputEl.value
+
+  let imperialLength = (inputNum * 3.281).toFixed(2)
+  let imperialVolume = (inputNum * 0.264).toFixed(2)
+  let imperialMass = (inputNum * 2.204).toFixed(2)
+  
+  let metricLength = (inputNum / 3.281).toFixed(2)
+  let metricVolume = (inputNum / 0.264).toFixed(2)
+  let metricMass = (inputNum / 2.204).toFixed(2)
+
+  lengthEl.textContent = `${inputNum} meters = ${imperialLength} feet | ${inputNum} feet = ${metricLength} meters`
+  
+  volumeEl.textContent = `${inputNum} meters = ${imperialVolume} feet | ${inputNum} feet = ${metricVolume} meters`
+  
+  massEl.textContent = `${inputNum} meters = ${imperialMass} feet | ${inputNum} feet = ${metricMass} meters`
+})
